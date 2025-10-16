b9c6d919b926cacd4d85d56d43841e90b8315a07
Rico Böhme <rico.boehme@robotron.de>
2025-10-16 12:49:43 +0200

    Einbindung_Weather-API

---
+++ netlify/functions/fetch-weather.js
@@ -0,0 +1,37 @@
+// netlify/functions/fetch-weather.js
+
+exports.handler = async function(event, context) {
+    const API_KEY = process.env.API_NINJAS_KEY;
+    const city = event.queryStringParameters.city;
+
+    if (!city) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Ein Stadtname ist erforderlich.' })
+        };
+    }
+
+    const API_ENDPOINT = `https://api.api-ninjas.com/v1/weather?city=${city}`;
+
+    try {
+        const response = await fetch(API_ENDPOINT, {
+            headers: { 'X-Api-Key': API_KEY }
+        });
+
+        if (!response.ok) {
+            return { statusCode: response.status, body: response.statusText };
+        }
+
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Interner Serverfehler' })
+        };
+    }
+};
